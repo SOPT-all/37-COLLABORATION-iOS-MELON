5866d8a1280cf9a96172ccd04d61614a2b0008c5
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-11-17 03:45:02 +0900

    [feat] '재생 중인 곡' 컴포넌트 구현

---
+++ Melon_iOS/Melon_iOS/Global/Components/NowPlayingTrackView.swift
@@ -0,0 +1,105 @@
+//
+//  NowPlayingTrackView.swift
+//  Melon_iOS
+//
+//  Created by 조영서 on 11/17/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class NowPlayingTrackView: UIView {
+    
+    // MARK: - UI Components
+    
+    private let checkboxButton = UIButton().then {
+        $0.setImage(UIImage(named: "ic_checkbox_default"), for: .normal)
+        $0.setImage(UIImage(named: "ic_checkbox_selected"), for: .selected)
+        $0.tintColor = .gray200
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    private let albumImageView = UIImageView().then {
+        $0.image = UIImage(named: "img_mixup9")
+        $0.layer.cornerRadius = 4
+        $0.clipsToBounds = true
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    private let musicTitleLabel = UILabel().then {
+        $0.text = "Blue Valentine"
+        $0.font = .pretendard(.body_m_14)
+        $0.textColor = .primary
+    }
+    
+    private let artistNameLabel = UILabel().then {
+        $0.text = "NMIXX"
+        $0.font = .pretendard(.caption_r_12)
+        $0.textColor = .gray200
+    }
+    
+    private let musicInfoStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.alignment = .leading
+    }
+    
+    private let menuIconView = UIImageView().then {
+        $0.image = UIImage(named: "ic_menu")?.withRenderingMode(.alwaysTemplate)
+        $0.tintColor = .gray200
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Methods
+    
+    private func setupUI() {
+        musicInfoStackView.addArrangedSubviews(musicTitleLabel, artistNameLabel)
+        
+        addSubviews(
+            checkboxButton,
+            albumImageView,
+            musicInfoStackView,
+            menuIconView
+        )
+    }
+    
+    private func setupLayout() {
+        
+        checkboxButton.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.centerY.equalToSuperview()
+            $0.size.equalTo(24)
+        }
+
+        albumImageView.snp.makeConstraints {
+            $0.centerY.equalTo(checkboxButton)
+            $0.leading.equalTo(checkboxButton.snp.trailing).offset(10)
+            $0.size.equalTo(42)
+        }
+
+        musicInfoStackView.snp.makeConstraints {
+            $0.centerY.equalTo(checkboxButton)
+            $0.leading.equalTo(albumImageView.snp.trailing).offset(10)
+            $0.trailing.equalTo(menuIconView.snp.leading).offset(-19)
+        }
+        
+        menuIconView.snp.makeConstraints {
+            $0.trailing.equalToSuperview()
+            $0.centerY.equalTo(checkboxButton)
+            $0.size.equalTo(24)
+        }
+    }
+}
