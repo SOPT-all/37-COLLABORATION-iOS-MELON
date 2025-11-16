d559775d06615b2fff8090434f7422a8700ad194
sumchive <haon0319@naver.com>
2025-11-17 01:47:47 +0900

    [refactor] 선택된 탭의 아이콘 자체를 교체

--- Melon_iOS/Melon_iOS/Presentation/Core/TabBarController.swift
+++ Melon_iOS/Melon_iOS/Presentation/Core/TabBarController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
-
+    
+    //MARK: - Properties
+    
     enum Tab: Int, CaseIterable {
         case home = 0
         case forYou
@@ -16,20 +18,20 @@ final class TabBarController: UITabBarController {
         case drawer
         case shortCut
         
-        var title: String {
-            switch self {
-            case .home: return "홈"
-            case .forYou: return "ForYou"
-            case .search: return "탐색"
-            case .drawer: return "음악서랍"
-            case .shortCut: return "바로가기"
-            }
-        }
-        
         var imageName: UIImage {
             switch self {
             case .home: return .btnHomeDefault
             case .forYou: return .btnForyouDefault
+            case .search: return .btnSearch
+            case .drawer: return .btnDrawer
+            case .shortCut: return .btnShortcut
+            }
+        }
+        
+        var selectedImageName: UIImage {
+            switch self {
+            case .home: return .btnHomeSelected
+            case .forYou: return .btnForyouSelected
             case .search: return .btnSearch
             case .drawer: return .btnDrawer
             case .shortCut: return .btnShortcut
@@ -43,32 +45,38 @@ final class TabBarController: UITabBarController {
             case .forYou:
                 return ViewController()
             case .search:
-                return ViewController()
+                return UIViewController()
             case .drawer:
-                return ViewController()
+                return UIViewController()
             case .shortCut:
-                return ViewController()
+                return UIViewController()
             }
         }
     }
     
+    //MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.delegate = self
         
         setViewControllers()
         setTabBarAppearance()
     }
     
+    //MARK: - Private Methods
+    
     private func setViewControllers() {
         self.viewControllers = Tab.allCases.map { tab in
             let vc = tab.viewController
-            let icon = resizeImage(image: tab.imageName)
             
-            vc.tabBarItem = UITabBarItem(
-                title: tab.title,
-                image: icon,
-                tag: tab.rawValue
-            )
+            let icon = resizeImage(image: tab.imageName).withRenderingMode(.alwaysOriginal)
+            let selectedIcon = resizeImage(image: tab.selectedImageName).withRenderingMode(.alwaysOriginal)
+            
+            let tabBarItem = UITabBarItem(title: nil, image: icon, selectedImage: selectedIcon)
+            tabBarItem.tag = tab.rawValue
+            vc.tabBarItem = tabBarItem
             return vc
         }
     }
@@ -80,42 +88,32 @@ final class TabBarController: UITabBarController {
         appearance.shadowColor = .gray600
         
         let itemAppearance = UITabBarItemAppearance()
-        itemAppearance.normal.iconColor = .gray300
-        itemAppearance.normal.titleTextAttributes = [
-            .foregroundColor: UIColor.gray300,
-            //            .font:
-            
-        ]
-        
-        itemAppearance.selected.iconColor = .primary
-        itemAppearance.selected.titleTextAttributes = [
-            .foregroundColor: UIColor.primary,
-            //            .font:
-        ]
         
         appearance.stackedLayoutAppearance = itemAppearance
-        
         tabBar.standardAppearance = appearance
         tabBar.scrollEdgeAppearance = appearance
     }
+    
+    
+    private func resizeImage(image: UIImage) -> UIImage {
+        let targetSize = CGSize(width: 24, height: 24)
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        let ratio = min(widthRatio, heightRatio)
+        let newSize = CGSize(width: size.width * ratio, height: size.height * ratio)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        image.draw(in: CGRect(origin: .zero, size: newSize))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage ?? image
+    }
 }
 
-private func resizeImage(image: UIImage) -> UIImage {
-    let targetSize = CGSize(width: 24, height: 24)
-    let size = image.size
-    
-    let widthRatio  = targetSize.width  / size.width
-    let heightRatio = targetSize.height / size.height
-    
-    let ratio = min(widthRatio, heightRatio)
-    let newSize = CGSize(width: size.width * ratio, height: size.height * ratio)
-    
-    UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
-    image.draw(in: CGRect(origin: .zero, size: newSize))
-    let newImage = UIGraphicsGetImageFromCurrentImageContext()
-    UIGraphicsEndImageContext()
-    
-    return newImage ?? image
 //MARK: - Extensions
 
 extension TabBarController: UITabBarControllerDelegate {
