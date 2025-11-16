35a4c1b973cc2af4d11b0bd03ff3a614295fedbb
Rudy-009 <rofxnaos@gmail.com>
2025-11-16 23:23:47 +0900

    [refactor/#7] gradient 함수 리펙토링

--- Melon_iOS/Melon_iOS/Global/Extensions/UIView+.swift
+++ Melon_iOS/Melon_iOS/Global/Extensions/UIView+.swift
@@ -9,30 +9,27 @@ import UIKit
 
 extension UIView {
 
-    func addSubviews(_ views: UIView...) {
-        views.forEach { self.addSubview($0) }
-    }
+  func addSubviews(_ views: UIView...) {
+    views.forEach { self.addSubview($0) }
+  }
   
-  func applyGradient3() {
+  func gradient3() {
+    self.layer.sublayers?
+      .filter { $0 is CAGradientLayer }
+      .forEach { $0.removeFromSuperlayer() }
+    
     let gradientLayer = CAGradientLayer()
-    
-    
     gradientLayer.colors = [
       UIColor(hexCode: "495263").cgColor,
       UIColor.gray500.cgColor,
       UIColor.background.cgColor
     ]
     
-    gradientLayer.locations = [
-      0.0,
-      0.53,
-      1.0
-    ]
-    
+    gradientLayer.locations = [ 0.0, 0.53, 1.0]
     gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
-    gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)   
+    gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
     
-    self.layer.addSublayer(gradientLayer)
+    self.layer.insertSublayer(gradientLayer, at: 0)
     gradientLayer.frame = self.bounds
   }
 }
