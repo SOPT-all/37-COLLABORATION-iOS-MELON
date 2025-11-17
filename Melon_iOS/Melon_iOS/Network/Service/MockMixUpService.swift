bd60477d130c7d6644c87b0736230d638f62b469
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-11-18 01:17:43 +0900

    [feat] 믹스업 목데이터 구현

--- Melon_iOS/Melon_iOS/Network/Service/MockMixUpService.swift
+++ Melon_iOS/Melon_iOS/Network/Service/MockMixUpService.swift
@@ -8,44 +8,44 @@
 import Foundation
 
 final class MockMixUpService {
-    func fetchSongs() async throws -> [MixUpSongResponseDTO] {
+    func fetchSongs() async throws -> [MixUpResponseDTO] {
         return Self.mockData
     }
 }
 
 extension MockMixUpService {
-    static let mockData: [MixUpSongResponseDTO] = [
-        MixUpSongResponseDTO(
+    static let mockData: [MixUpResponseDTO] = [
+        MixUpResponseDTO(
             title: "Ditto",
             artist: "NewJeans",
             imageUrl: "https://image.bugsm.co.kr/album/images/500/40824/4082425.jpg"
         ),
-        MixUpSongResponseDTO(
+        MixUpResponseDTO(
             title: "Love Dive",
             artist: "IVE",
             imageUrl: "https://image.bugsm.co.kr/album/images/500/40737/4073710.jpg"
         ),
-        MixUpSongResponseDTO(
+        MixUpResponseDTO(
             title: "Seven",
             artist: "정국",
             imageUrl: "https://image.bugsm.co.kr/album/images/350/40889/4088913.jpg"
         ),
-        MixUpSongResponseDTO(
+        MixUpResponseDTO(
             title: "Hype Boy",
             artist: "NewJeans",
             imageUrl: "https://image.bugsm.co.kr/album/images/500/40780/4078016.jpg"
         ),
-        MixUpSongResponseDTO(
+        MixUpResponseDTO(
             title: "Super Shy",
             artist: "NewJeans",
             imageUrl: "https://image.bugsm.co.kr/album/images/500/40885/4088574.jpg"
         ),
-        MixUpSongResponseDTO(
+        MixUpResponseDTO(
             title: "I AM",
             artist: "IVE",
             imageUrl: "https://image.bugsm.co.kr/album/images/500/40849/4084947.jpg"
         ),
-        MixUpSongResponseDTO(
+        MixUpResponseDTO(
             title: "ANTIFRAGILE",
             artist: "LE SSERAFIM",
             imageUrl: "https://image.bugsm.co.kr/album/images/500/40807/4080706.jpg"
