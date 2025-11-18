a0d7c492a8de32a8c75b20d693c6f8632bdfe6ea
Rudy-009 <rofxnaos@gmail.com>
2025-11-18 22:41:39 +0900

    [feat/#26] UICollectionView 익스텐션 코드 작성

---
+++ Melon_iOS/Melon_iOS/Global/Empties/EmptyCollectionViewCell.swift
@@ -0,0 +1,10 @@
+//
+//  EmptyCollectionViewCell.swift
+//  Melon_iOS
+//
+//  Created by 이승준 on 11/18/25.
+//
+
+import UIKit
+
+class EmptyCollectionViewCell: BaseUICollectionViewCell, ReuseIdentifiable {  }
---
+++ Melon_iOS/Melon_iOS/Global/Extensions/UICollectionView+.swift
@@ -0,0 +1,45 @@
+//
+//  UICollectionView.swift
+//  Melon_iOS
+//
+//  Created by 이승준 on 11/18/25.
+//
+
+import UIKit
+
+extension UICollectionView {
+  
+  func cellRegister<T: UICollectionViewCell & ReuseIdentifiable>(_ type: T.Type) {
+    self.register(type.self, forCellWithReuseIdentifier: type.reuseIdentifier)
+  }
+  
+  func headerRegister<T: UICollectionReusableView & ReuseIdentifiable>(_ type: T.Type) {
+    self.register(
+      type.self,
+      forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+      withReuseIdentifier: type.reuseIdentifier
+    )
+  }
+  
+  func footerRegister<T: UICollectionReusableView & ReuseIdentifiable>(_ type: T.Type) {
+    self.register(
+      type.self,
+      forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter,
+      withReuseIdentifier: type.reuseIdentifier
+    )
+  }
+  
+  func dequeueReusableCell<T: UICollectionViewCell & ReuseIdentifiable>(
+    _ cellType: T.Type,
+    for indexPath: IndexPath
+  ) -> T {
+    guard let cell = self.dequeueReusableCell(
+        withReuseIdentifier: cellType.reuseIdentifier,
+        for: indexPath
+    ) as? T else {
+      return EmptyCollectionViewCell() as! T
+    }
+    return cell
+  }
+  
+}
