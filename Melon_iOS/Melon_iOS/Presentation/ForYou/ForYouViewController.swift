ff0e97213042b2d28051aa3807545d8b558a08a8
sumchive <haon0319@naver.com>
2025-11-24 21:00:54 +0900

    [feat] 오늘의추천 컬렉션뷰 셀 등록

--- Melon_iOS/Melon_iOS/Presentation/ForYou/ForYouViewController.swift
+++ Melon_iOS/Melon_iOS/Presentation/ForYou/ForYouViewController.swift
@@ -10,17 +10,21 @@ import UIKit
 import SnapKit
 import Then
 
-final class ForYouViewController: BaseViewController {
+final class ForYouViewController: BaseViewController, UICollectionViewDelegate {
     
     // MARK: - Properties
     
     private let forYouView = ForYouView()
+    
+    let basedTasteItems = RecommendationItem.basedTaste
+    let basedSituationItems = RecommendationItem.basedSituation
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupCollectionView()
         setMallangListenButtonAction()
     }
     
@@ -36,6 +40,19 @@ final class ForYouViewController: BaseViewController {
         }
     }
     
+    private func setupCollectionView() {
+        let rv = forYouView.recommendView
+//        let cv = forYouView.customSongView.collectionView
+        
+        rv.tasteCollectionView.delegate = self
+        rv.tasteCollectionView.dataSource = self
+        rv.situationCollectionView.delegate = self
+        rv.situationCollectionView.dataSource = self
+        
+//        cv.delegate = self
+//        cv.dataSource = self
+    }
+    
     // MARK: - Private Methods
     
     private func setMallangListenButtonAction () {
@@ -43,9 +60,72 @@ final class ForYouViewController: BaseViewController {
             let toast = ToastMessage()
             self?.view.addSubview(toast)
             toast.configure(action: {
-                print("toast message action button tapped")
+                
             })
             toast.show()
         }
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension ForYouViewController: UICollectionViewDataSource {
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let rv = forYouView.recommendView
+//        let cv = forYouView.customSongView.collectionView
+        
+        if collectionView == rv.tasteCollectionView {
+            return basedTasteItems.count
+        } else if collectionView == rv.situationCollectionView {
+            return basedSituationItems.count
+        }
+//        else if collectionView == cv {
+//            return customSongItems.count
+//        }
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cv = forYouView.customSongView.collectionView
+        
+//        if collectionView == cv {
+//            guard let cell = collectionView.dequeueReusableCell(
+//                withReuseIdentifier: CustomSongViewCell.reuseIdentifier,
+//                for: indexPath
+//            ) as? CustomSongViewCell else {
+//                return UICollectionViewCell()
+//            }
+//            let item = customSongItems[indexPath.item]
+//            
+//            cell.configure(
+//                title: item.title,
+//                artist: item.artist,
+//                imageUrl: item.imageUrl
+//            )
+//            return cell
+//        }
+        
+//        else {
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: RecommendViewCell.reuseIdentifier,
+                for: indexPath
+            ) as? RecommendViewCell else {
+                return UICollectionViewCell()
+            }
+            
+            let recommendView = forYouView.recommendView
+            let item: RecommendationItem
+            
+            if collectionView == recommendView.tasteCollectionView {
+                item = basedTasteItems[indexPath.item]
+            } else if collectionView == recommendView.situationCollectionView {
+                item = basedSituationItems[indexPath.item]
+            } else {
+                return UICollectionViewCell()
+            }
+            
+            cell.configure(content: item.content, hasImage: item.hasImage)
+            return cell
+//        }
     }
 }
