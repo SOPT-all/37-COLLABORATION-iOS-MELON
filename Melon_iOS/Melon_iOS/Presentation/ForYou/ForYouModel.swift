3c02c9f2ef02516074c4faa046b9543cfffea062
sumchive <haon0319@naver.com>
2025-11-24 20:58:38 +0900

    [feat] '오늘의 추천' 구조체 구현

--- Melon_iOS/Melon_iOS/Presentation/ForYou/ForYouModel.swift
+++ Melon_iOS/Melon_iOS/Presentation/ForYou/ForYouModel.swift
@@ -5,4 +5,20 @@
 //  Created by 조영서 on 11/15/25.
 //
 
-// 여기에쓰거라
+
+struct RecommendationItem {
+    let content: String
+    let hasImage: Bool
+    
+    static let basedTaste: [RecommendationItem] = [
+        RecommendationItem(content: "감성보컬", hasImage: false),
+        RecommendationItem(content: "pH-1", hasImage: true),
+        RecommendationItem(content: "그르부있는비트", hasImage: false)
+    ]
+    
+    static let basedSituation: [RecommendationItem] = [
+        RecommendationItem(content: "드라이브", hasImage: false),
+        RecommendationItem(content: "집중할 때", hasImage: false),
+        RecommendationItem(content: "조용한 밤", hasImage: false)
+    ]
+}
