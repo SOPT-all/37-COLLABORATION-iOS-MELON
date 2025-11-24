db35ee2d6bde25f6157d0f7fe3933ca33e70e1b2
sumchive <haon0319@naver.com>
2025-11-24 20:58:57 +0900

    [feat] 오늘의 추천 cell 구현

---
+++ Melon_iOS/Melon_iOS/Presentation/ForYou/RecommendViewCell.swift
@@ -0,0 +1,140 @@
+//
+//  RecommendViewCell.swift
+//  Melon_iOS
+//
+//  Created by mandoo on 11/24/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+import Kingfisher
+
+final class RecommendViewCell: BaseUICollectionViewCell, ReuseIdentifiable {
+    
+    // MARK: - Properties
+    
+    var isSelectedCell: Bool = false {
+        didSet {
+            updateStyle()
+        }
+    }
+    
+    var hasImage: Bool = false {
+        didSet {
+            updateLayout()
+        }
+    }
+    
+    // MARK: - UI Components
+    
+    private let chipButton = UIButton().then {
+        $0.layer.cornerRadius = 24
+        $0.clipsToBounds = true
+    }
+    
+    
+    private let artistImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.image = .imgToday
+        $0.layer.cornerRadius = 18
+        $0.clipsToBounds = true
+        $0.isUserInteractionEnabled = false
+    }
+    
+    private let contentLabel = UILabel().then {
+        $0.font = .pretendard(.body_m_14)
+        $0.textColor = .white
+        $0.textAlignment = .center
+        $0.isUserInteractionEnabled = false
+    }
+    
+    private lazy var contentStackView = UIStackView(
+        arrangedSubviews: [artistImageView, contentLabel]
+    ).then {
+        $0.axis = .horizontal
+        $0.alignment = .center
+        $0.spacing = 8.0
+        $0.isUserInteractionEnabled = false
+    }
+    
+    // MARK: - Setup Methods
+    
+    override func setUI() {
+        
+        addSubviews(
+            chipButton
+        )
+        
+        chipButton.addSubviews(contentStackView)
+        
+        chipButton.addTarget(self, action: #selector(chipTapped), for: .touchUpInside)
+        
+        updateStyle()
+        updateLayout()
+    }
+    
+    override func setLayout() {
+        chipButton.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.height.equalTo(48)
+        }
+        
+        artistImageView.snp.makeConstraints {
+            $0.size.equalTo(36)
+        }
+        
+        contentStackView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @objc func chipTapped() {
+        isSelectedCell.toggle()
+    }
+}
+
+// MARK: - Extensions
+
+extension RecommendViewCell {
+    
+    private func updateStyle() {
+        if isSelectedCell {
+            chipButton.backgroundColor = .secondary
+            chipButton.layer.borderWidth = 0
+            contentLabel.textColor = .white
+        } else {
+            chipButton.backgroundColor = .none
+            chipButton.layer.borderWidth = 0.3
+            chipButton.layer.borderColor = UIColor.gray400.cgColor
+            contentLabel.textColor = .gray100
+        }
+    }
+    
+    private func updateLayout() {
+        artistImageView.isHidden = !hasImage
+        let leftSpace: CGFloat
+        let rightSpace: CGFloat
+        
+        if hasImage {
+            leftSpace = 4.0
+            rightSpace = 12.0
+        } else {
+            leftSpace = 16.0
+            rightSpace = 16.0
+        }
+        contentStackView.snp.remakeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(chipButton.snp.leading).offset(leftSpace)
+            $0.trailing.equalTo(chipButton.snp.trailing).offset(-rightSpace)
+        }
+    }
+    
+    func configure(content: String, hasImage: Bool) {
+        contentLabel.text = content
+        self.hasImage = hasImage
+    }
+}
